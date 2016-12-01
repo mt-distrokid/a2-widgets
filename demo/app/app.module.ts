33b84921c76505dcac57c7de573be7b71df7d940
mrthomps <michael.thompson@springleaf.com>
2016-12-01 12:07:19 -0600

    1.0.17

---
+++ demo/app/app.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule }   from '@angular/router';
+
+import { HttpModule } from '@angular/http';
+import { FormsModule } from '@angular/forms';
+
+import { WidgetsModule, WidgetsComponent } from '../../node_modules/a2-widgets/widgets.module' // to implement widgets
+
+import { AppComponent } from './app.component';
+
+import 'rxjs/add/operator/toPromise';
+import { HelloComponent } from './hello/hello.component';
+
+export const appCentral = {
+    routeTable: [
+        {path: 'hello', component: HelloComponent},
+        {path: '', component: WidgetsComponent}, {path: '**', component: WidgetsComponent} // default
+    ]
+};
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule,
+    WidgetsModule.setOptions(
+      { 
+        widgets_url: "./app/resources/data/widgets.json",
+        components: {
+          'app-hello': HelloComponent
+        }
+      }
+    ),
+    RouterModule.forRoot(appCentral.routeTable, {useHash: true}),
+  ],
+  declarations: [
+    AppComponent,
+    HelloComponent
+  ],
+  providers: [],
+  entryComponents: [
+    HelloComponent
+  ],
+  bootstrap: [AppComponent]
+})
+
+export class AppModule {}
